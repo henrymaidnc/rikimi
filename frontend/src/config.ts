72da712a7848c3ddc923701ef8ee6bb05ef8c390
Trinh <cheltonmai@gmail.com>
2025-06-09 14:53:21 +0700

    Prod - Setup prod for FE

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,7 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const API_BASE_URL = isDevelopment 
+export const API_BASE_URL = 'http://13.220.164.76/api';
+
+export const API_BASE_URL_DEV = isDevelopment 
   ? 'http://localhost:8009/api'
   : 'http://your-ec2-public-ip/api'; // Replace with your EC2 IP 
