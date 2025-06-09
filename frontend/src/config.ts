4cf7d2c93a255564877c7a4ca3af3e03fce25e05
Trinh <cheltonmai@gmail.com>
2025-06-09 15:08:13 +0700

    Prod - Add domain name

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,8 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const API_BASE_URL = 'http://13.220.164.76/api';
+export const API_BASE_URL = 'https://www.rikimi.edu.vn/api';
 
 export const API_BASE_URL_DEV = isDevelopment 
   ? 'http://localhost:8009/api'
-  : 'http://your-ec2-public-ip/api'; // Replace with your EC2 IP 
+  : 'https://www.rikimi.edu.vn/api'; // Replace with your EC2 IP 
+  
